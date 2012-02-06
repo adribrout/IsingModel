ccfae0f751af42176ec6a5ae60ad7c1ae37f395f
Eté Rémi <remi.ete@gmail.com>
2012-02-07 00:14:31 +0100

    Configure file begun.

---
+++ Configure.sh
@@ -0,0 +1,25 @@
+#######################################################################
+#
+#//Ising Model Project part of M1 computing project
+#
+#Copyright (c) 2012 Rémi Eté & Adrien Girard,
+#All Right Reserved.
+#
+#Use and copying of this software and preparation of derivative works
+#based upon this software are permitted. Any copy of this software or
+#of any derivative work must include the above copyright notice, this
+#paragraph and the one after it.
+#
+#######################################################################
+#
+#This script setups all environement variables needed to install,
+#compile and run the program.
+#Used via 'source env.sh' in Configure file
+#
+#######################################################################
+
+#!/bin/bash
+
+me=$0
+
+
