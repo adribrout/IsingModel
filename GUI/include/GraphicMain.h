e7a90d50925934ed1562a31fc3116d4c72f34ad2
Eté Rémi <remi.ete@gmail.com>
2012-02-10 23:41:50 +0100

    GraphicMain.h and .C added

---
+++ GUI/include/GraphicMain.h
@@ -0,0 +1,23 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice, this
+ * paragraph and the one after it.
+ *
+ *
+ */
+
+#ifndef GRAPHICMAIN_H
+#define GRAPHICMAIN_H
+
+#include <QApplication>
+#include <QPushButton>
+
+int GraphicMain(int argc, char * argv[]) ;
+
+#endif   // GRAPHICMAIN
---
+++ GUI/src/GraphicMain.C
@@ -0,0 +1,27 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice, this
+ * paragraph and the one after it.
+ *
+ *
+ */
+
+
+#include "../include/GraphicMain.h"
+
+using namespace std ;
+
+int GraphicMain(int argc, char * argv[])
+{
+	QApplication app(argc, argv) ;
+	QPushButton bouton("Salut les Zéros, la forme ?") ;
+    bouton.setText("Pimp mon bouton !") ;
+    bouton.show() ;
+    return  app.exec() ;
+}
