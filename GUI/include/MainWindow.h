6eebfaa7bc0c3fb282e77aef7a2b2bb8645c8d9e
Eté Rémi <remi.ete@gmail.com>
2012-02-13 16:48:22 +0100

    inherit from QWidget instead of QMainWindow (must be changed...)

---
+++ GUI/include/MainWindow.h
@@ -0,0 +1,38 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice, this
+ * paragraph and the one after it.
+ *
+ *
+ */
+
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui>
+
+class MainWindow : public QWidget
+{
+	Q_OBJECT
+
+private:
+
+public slots :
+
+
+
+public :
+
+	MainWindow() ;
+	~MainWindow() ;
+
+};
+
+#endif  // MAINWINDOW_H
