69e45236a069c1ec814b88d94a76bf60e6adbd74
Eté Rémi <remi.ete@gmail.com>
2012-02-13 18:30:22 +0100

    moved

---
+++ GUI/GraphicMain.C
@@ -0,0 +1,31 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice, this
+ * paragraph and the one after it.
+ *
+ *
+ */
+
+
+#include <QApplication>
+#include <QtGui>
+#include "GraphicMain.h"
+//#include "MainWindow/include/MainWindow.h"
+#include "./gnuplot/GnuplotWindow/include/GnuplotWindow.h"
+
+using namespace std ;
+
+int GraphicMain(int argc, char * argv[])
+{
+	QApplication app(argc, argv) ;
+	GnuplotWindow gnuWin ;
+	gnuWin.show() ;
+
+	return  app.exec() ;
+}
---
+++ GUI/GraphicMain.h
@@ -0,0 +1,21 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice, this
+ * paragraph and the one after it.
+ *
+ *
+ */
+
+#ifndef GRAPHICMAIN_H
+#define GRAPHICMAIN_H
+
+
+int GraphicMain(int argc, char * argv[]) ;
+
+#endif   // GRAPHICMAIN
