c1e8a277af9a01221390bea17ad9e9c2665c96ee
Eté Rémi <remi.ete@gmail.com>
2012-02-14 02:01:28 +0100

    RangeBox and PlotTabView added. To fill!!

---
+++ GUI/gnuplot/NewFile/include/RangeGroupBox.h
@@ -0,0 +1,31 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice.
+ *
+ *
+ */
+
+#ifndef RANGEGROUPBOX_H
+#define RANGEGROUPBOX_H
+
+#include <QtGui>
+
+class RangeGroupBox : public QGroupBox
+{
+
+public :
+	RangeGroupBox() ;
+	~RangeGroupBox() ;
+
+private:
+
+
+};
+
+#endif  // RANGEGROUPBOX_H
---
+++ GUI/gnuplot/NewFile/src/PlotTableView.C
@@ -0,0 +1,24 @@
+/*
+ *
+ * //Ising Model Project part of M1 computing project
+ * Copyright (c) 2012 Rémi Eté & Adrien Girard,
+ * All Right Reserved.
+ *
+ * Use and copying of this software and preparation of derivative works
+ * based upon this software are permitted. Any copy of this software or
+ * of any derivative work must include the above copyright notice.
+ *
+ *
+ */
+
+#include "../include/PlotTableView.h"
+
+PlotTableView::PlotTableView()
+{
+
+}
+
+PlotTableView::~PlotTableView()
+{
+
+}
